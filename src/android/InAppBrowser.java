79412e2b73d88d042a58af9f64c2c9328f3786ce
Yiğit Serin <yigits@yandex.com>
2024-08-13 10:57:13 +0300

    Update InAppBrowser.java
    Share schema added.
    Download file added.

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.os.Environment;
 import android.os.Parcelable;
 import android.provider.Browser;
 import android.content.res.Resources;
@@ -35,6 +36,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Base64;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.KeyEvent;
@@ -62,6 +64,8 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.core.content.FileProvider;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.Config;
 import org.apache.cordova.CordovaArgs;
@@ -74,9 +78,15 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -1210,6 +1220,54 @@ public class InAppBrowser extends CordovaPlugin {
                     LOG.e(LOG_TAG, "Error with " + url + ": " + e.toString());
                 }
             }
+            else if(url.startsWith("share:")) {
+
+              String base64EncodedPart = url.substring("share://".length());
+
+              byte[] decodedBytes = Base64.decode(base64EncodedPart, Base64.DEFAULT);
+              String decodedString = new String(decodedBytes);
+
+
+              JSONObject jsonObject = new JSONObject();
+              try {
+                // JSON parse işlemi
+                jsonObject = new JSONObject(decodedString);
+              } catch (Exception e) {
+
+              }
+              // JSON'dan değer okuma
+              String title = jsonObject.optString("title", null);
+              String text = jsonObject.optString("text", null);
+              String shareUrl = jsonObject.optString("url", null);
+              String fileName = jsonObject.optString("fileName", null);
+              String fileUrl = jsonObject.optString("fileUrl", null);
+              String mimeType = jsonObject.optString("mimeType", null);
+
+              new Thread(new Runnable() {
+                @Override
+                public void run() {
+                  Uri fileUri = downloadFile(cordova.getContext(), fileUrl, fileName);
+
+                  // UI thread'inde paylaşım intentini başlat
+                  if (fileUri != null) {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType(mimeType); // Paylaşılan dosyanın MIME türü
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, title + " " + text);
+                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                    // UI thread'de çalıştırmak için runOnUiThread kullanın
+                    cordova.getActivity().runOnUiThread(new Runnable() {
+                      @Override
+                      public void run() {
+                        cordova.getActivity().startActivity(Intent.createChooser(shareIntent, title + " " + text));
+                      }
+                    });
+                  }
+                }
+              }).start();
+              override = true;
+            }
             // If sms:5551212?body=This is the message
             else if (url.startsWith("sms:")) {
                 try {
@@ -1458,5 +1516,57 @@ public class InAppBrowser extends CordovaPlugin {
             // By default handle 401 like we'd normally do!
             super.onReceivedHttpAuthRequest(view, handler, host, realm);
         }
-    }
+
+        public Uri downloadFile(Context context, String fileUrl, String fileName) {
+          Uri fileUri = null;
+          InputStream inputStream = null;
+          FileOutputStream outputStream = null;
+
+          File tempFile = null;
+          try {
+            // URL'yi ve bağlantıyı aç
+            URL url = new URL(fileUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            // Geçici dosyayı oluştur
+            File cacheDir = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+            if (!cacheDir.exists()) {
+              cacheDir.mkdirs(); // Klasör yoksa oluştur
+            }
+            tempFile = new File(cacheDir, fileName);
+
+            // InputStream ile veri oku ve dosyaya yaz
+            inputStream = connection.getInputStream();
+            outputStream = new FileOutputStream(tempFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+              outputStream.write(buffer, 0, bytesRead);
+            }
+
+            // Dosyanın URI'sini al
+            String authority = context.getPackageName() + ".sharing.provider"; // Dinamik paket adı
+            fileUri = FileProvider.getUriForFile(context, authority, tempFile);
+
+          } catch (Exception e) {
+            e.printStackTrace();
+          } finally {
+            // InputStream ve OutputStream kapatma
+            try {
+              if (outputStream != null) {
+                outputStream.close();
+              }
+              if (inputStream != null) {
+                inputStream.close();
+              }
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+
+          return fileUri;
+        }
+      }
 }
